b6a04865a11d024af1b6944f0e04afc67044251d
Adwaith <adwaith3208@gmail.com>
2019-08-23 19:49:20 +0530

    Added funding and thread routes

--- routes/project.js
+++ routes/project.js
@@ -100,20 +100,123 @@ projectRouter.post("/:id/accept/:userid", function (req, res) {
                     console.log("Error or User with the given id does not exist.");
                     res.json({ success: false });
                 }
-                project.requests = project.requests.filter(preq => {
-                    return preq.toString() != req.params.userid;
-                })
-                project.members.push(user._id);
 
-                project.save(function (err) {
-                    if (err) {
-                        console.log("Error in saving updated project");
-                        res.json({ success: false });
-                    }
-                    else {
-                        res.json({ success: true });
-                    }
+                let flag=false;
+                project.requests=project.requests.filter(preq => {
+                    flag=(flag||(preq.toString()==req.params.userid));
+                    return preq.toString()!=req.params.userid;
                 })
+                if(!flag){
+                    res.json({success: false});
+                }
+                else{
+                    project.members.push(user._id);
+                    project.save(function(err){
+                        if(err){
+                            console.log("Error in saving updated project");
+                            res.json({success: false});
+                        }
+                        else {
+                            res.json({success: true});
+                        }
+                    })
+                }
+            })
+        }
+    })
+})
+
+projectRouter.post('/:id/fund', function(req, res){
+    Project.findById(req.params.id, function(err, project){
+        if(err||!project){
+            console.log("Error or Project with the given id does not exists.");
+            res.json({success: false});
+        }
+        else{
+            var fundObj = new Object();
+            fundObj.owner = req.user;
+            fundObj.amount = req.body.amount;
+            fundObj.desc = req.body.desc;
+            project.fundProp.push(fundObj);
+            project.save(function(err){
+                if(err){
+                    console.log("Error in saving project");
+                    res.json({success: false});
+                }
+                else{
+                    res.json({success: true});
+                }
+            })
+        }
+    })
+})
+
+projectRouter.post("/:id/acceptProp/:userid", function(req, res){
+    Project.findById(req.params.id, function(err, project){
+        if(err||!project){
+            console.log("Error or Project with the given id does not exists.");
+            res.json({success: false});
+        }
+        else if(req.user._id.toString()!=project.owner.toString()){
+            console.log("Request not made by owner of the project.");
+            res.json({success: false});
+        }
+        else{
+            User.findById(req.params.userid, function(err, user){
+                if(err||!user){
+                    console.log("Error or User with the given id does not exist.");
+                    res.json({success:false});
+                }
+                let flag=false;
+                let amt=0;
+                project.fundProp=project.fundProp.filter(freq => {
+                    if(freq.owner.toString()==req.params.userid){
+                        flag=true;
+                        amt=freq.amount;
+                    }
+                    return preq.toString()!=req.params.userid;
+                })
+                if(!flag){
+                    res.json({success: false});
+                }
+                else{
+                    project.funders.push(user._id);
+                    project.fundRecv+=amt;
+                    project.save(function(err){
+                        if(err){
+                            console.log("Error in saving updated project");
+                            res.json({success: false});
+                        }
+                        else {
+                            res.json({success: true});
+                        }
+                    })
+                }
+
+            })
+        }
+    })
+})
+
+projectRouter.post('/:id/comment', function(req, res){
+    Project.findById(req.body.id, function(err, project){
+        if(err||!project){
+            console.log("Error or Project not found");
+            res.json({success: false});
+        }
+        else{
+            let msg = new Object();
+            msg.owner= req.user;
+            msg.message = req.body.message;
+            project.thread.push(msg);
+            project.save(function(err){
+                if(err){
+                    console.log("Error in saving project");
+                    res.json({success: false});
+                }
+                else{
+                    res.json({success: true});
+                }
             })
         }
     })
