45dd2179543bbf5b2d13539e2496f63a50e35043
Adwaith <adwaith3208@gmail.com>
2019-08-24 11:15:59 +0530

    Add user counts

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,43 @@
 const userRouter = require('express').Router();
 var User = require('../models/user');
+var Project = require('../models/project');
+
 const { isLoggedIn } = require('../middlewares/auth');
 
 userRouter.get('/:id', function(req, res){
     User.findById(req.params.id, function(err, user){
         if(err||!user){
             console.log("Error in finding user");
+            res.json({success: false});
         }
         else{
+            let po=0, ppo=0;
+            Project.find()
+                .populate('owner')
+                .populate('members').exec(function(err, projects){
+                    console.log(req.params)
+                    if(err){
+                        console.log("Error in finding user");
+                        res.json({success: false});
+                    }
+                    else{
+                        for(let i=0; i<projects.length; i++){
+                            if(projects[i].owner._id.toString()==req.params.id){
+                                po++;
+                            }
+
+                            for(let j=0; j<projects[i].members.length; j++){
+                                if(projects[i].members[j]._id.toString()==req.params.id){
+                                    ppo++;
+                                }
+                            }
+                        };
+                        console.log('po:', po, 'ppo:', ppo);
+                        res.render('show-user', {user, projectsOwned: po, projectsPartOf: ppo})
+                    }
+                })
+
+
             res.render('show-user',{user});
         }
     })
