b305105f59fceefc0f1f053f68bed4e8317db7d8
Subash Aravindan <p.subasharavindan@gmail.com>
2019-08-24 11:30:04 +0530

    Fix bug in users page

--- routes/users.js
+++ routes/users.js
@@ -4,43 +4,39 @@ var Project = require('../models/project');
 
 const { isLoggedIn } = require('../middlewares/auth');
 
-userRouter.get('/:id', function(req, res){
-    User.findById(req.params.id, function(err, user){
-        if(err||!user){
+userRouter.get('/:id', function (req, res) {
+    const { id } = req.params;
+    User.findById(id, function (err, user) {
+        if (err || !user) {
             console.log("Error in finding user");
-            res.json({success: false});
+            res.json({ success: false });
         }
-        else{
-            let po=0, ppo=0;
+        else {
+            let po = 0, ppo = 0;
             Project.find()
                 .populate('owner')
-                .populate('members').exec(function(err, projects){
-                    console.log(req.params)
-                    if(err){
+                .populate('members').exec(function (err, projects) {
+                    if (err) {
                         console.log("Error in finding user");
-                        res.json({success: false});
+                        res.json({ success: false });
                     }
-                    else{
-                        for(let i=0; i<projects.length; i++){
-                            if(projects[i].owner._id.toString()==req.params.id){
+                    else {
+                        for (let i = 0; i < projects.length; i++) {
+                            if (projects[i].owner._id.toString() == id) {
                                 po++;
                             }
 
-                            for(let j=0; j<projects[i].members.length; j++){
-                                if(projects[i].members[j]._id.toString()==req.params.id){
+                            for (let j = 0; j < projects[i].members.length; j++) {
+                                if (projects[i].members[j]._id.toString() == id) {
                                     ppo++;
                                 }
                             }
                         };
-                        console.log('po:', po, 'ppo:', ppo);
-                        res.render('show-user', {user, projectsOwned: po, projectsPartOf: ppo})
+                        res.render('show-user', { user: user, projectsOwned: po, projectsPartOf: ppo })
                     }
                 })
-
-
-            res.render('show-user',{user});
         }
     })
 })
 
-module.exports=userRouter;
+module.exports = userRouter;
